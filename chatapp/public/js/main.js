00d93d9fe7338b0f6a544834ec30185493d42c50
rishavkrmee <123080627+rishavkrmee@users.noreply.github.com>
2023-09-09 20:15:46 +0530

    final commit

--- chatapp/public/js/main.js
+++ chatapp/public/js/main.js
@@ -1,4 +1,9 @@
+// Utility functions
 
+/**
+* @param {number} length - The length of user id.
+* @returns {string} A random string generated
+*/
 generateRandomString = (length)=>{
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?';
   let randomString = '';
@@ -11,6 +16,7 @@ generateRandomString = (length)=>{
   return randomString;
 }
 
+
 function getUsername(){
   user_name = prompt("Please enter your name:");
   if (!user_name) {
@@ -19,14 +25,22 @@ function getUsername(){
   _id = generateRandomString(6);
 }
 
+//---------------------------------************************-----------------------//
+/**
+* @param {number} msg - message object sent by server to all clients
+*/
 
 function handleMessage(msg) {
     // Create the main div with class "msg-bubble"
     const msgBubble = document.createElement('div');
     msgBubble.classList.add('msg-bubble');
+
+    // checking whether the message sent by server was sent by me or not
+    // If sent by then adding a class called right-msg to msgBubble
     if (msg.user_id === _id) {
       msgBubble.classList.add('right-msg');
     }
+    // If not sent by me then adding a class called left-msg to msgBubble
     else{
       msgBubble.classList.add('left-msg');
     }
@@ -39,6 +53,7 @@ function handleMessage(msg) {
     const msgText = document.createElement('div');
     msgText.classList.add('msg-text');
     msgText.textContent = msg.text;
+
     // Create the "msg-info-name" span
     const msgInfoName = document.createElement('span');
     msgInfoName.classList.add('msg-info-name');
@@ -47,57 +62,22 @@ function handleMessage(msg) {
     // Create the "msg-info-time" span
     const msgInfoTime = document.createElement('span');
     msgInfoTime.classList.add('msg-info-time');
-    msgInfoTime.textContent = Date.now();
+    today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes();
+   
+    msgInfoTime.textContent = time;
     msgInfo.appendChild(msgInfoName);
     msgInfo.appendChild(msgInfoTime);
     msgBubble.appendChild(msgInfo);
     msgBubble.appendChild(msgText);
     messages.appendChild(msgBubble);
-
-
-    if (msg.fileName) {
-  
-      // This is a file message (an image in this case)
-      var item1 = document.createElement('li');
-      var imageElement = document.createElement('img');
-      imageElement.src = msg.fileData;
-      item1.appendChild(imageElement);
-      item1.classList.add('.msg-img');
-      if (msg.fileId === _id) {
-        
-      item1.classList.remove('left-msg');
-      item1.classList.add('right-msg');
-      
-    }
-    messages.appendChild(item1);
-    } 
-
     const scrollableDiv = document.getElementsByClassName('msger');
     scrollableDiv.scrollTop = scrollableDiv.scrollHeight;
 
     }
 
 
-    function handleFileSelect(event) {
-    const file = event.target.files[0];
-    if (file) {
-    const reader = new FileReader();
-    console.log("file select");
-    reader.onload = (e) => {
-      const fileData = e.target.result;
-      const message = {
-              username: user_name,
-              text: input.value,
-              fileId: _id,
-              fileData: fileData, 
-              fileName: file.name, 
-            };
-    socket.emit('chat message', message);
-    input.value = '';};
-
-    reader.readAsDataURL(file); }
-    }
-
+    // assigning variables
     var socket = io();
     var user_name;
     var _id;
@@ -105,13 +85,10 @@ function handleMessage(msg) {
     var form = document.getElementById('form');
     var input = document.getElementById('input');
 
-    // // var generateRandomString = utils.generateRandomString;
-    // var getUsername = utils.getUsername;
-    // var handleMessage = utils.handleMessage;
-    // var handleFileSelect = utils.handleFileSelect;
-
+    // When the window loads, run the get username function
     window.onload = getUsername;
 
+    //  When we submit the file, then emit 
     form.addEventListener('submit', function(e) {
       e.preventDefault();
       if (input.value) {
@@ -119,17 +96,10 @@ function handleMessage(msg) {
           username: user_name,
           text: input.value,
           user_id: _id,
-          fileData: null, // Add the file data to the message
-          fileName: null, // Add the file name to the message
         };
         socket.emit('chat message', message);
         input.value = '';
       }
 });
-
-const fileInput = document.getElementById('fileInput');
-fileInput.addEventListener('change', handleFileSelect);
-    // Function to handle file selection
-
 
 socket.on('chat message', handleMessage);
--- chatapp/server.js
+++ chatapp/server.js
@@ -5,21 +5,17 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const utils = require('./public/js/utils')
 app.get('/', (req, res) => {
-  res.sendFile('/Users/rishavkumar/Desktop/petrichor/project/frontend/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 app.use(express.static('public'));
 io.on('connection', (socket) => {
-    // let _id = utils.generateRandomString(6);
-    // io.emit('new_user');
   socket.on('chat message', (msg) => {
-    console.log(msg);
     io.emit('chat message', msg);
   });
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log('Listening on port 3000');
 });
 
